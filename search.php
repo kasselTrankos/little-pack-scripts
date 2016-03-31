63fc0538c5635780fcedd7c35efcbd43a4d34835
kasselTrankos <alvaro.touzon@gmail.com>
2016-03-31 19:54:52 +0200

    next little

--- search.php
+++ search.php
@@ -1,3 +1,23 @@
 <?php
-  echo "hola";
- ?>
+echo "bienvenido, vamos a ver que string buscar:\n";
+$stdin = fopen('php://stdin', 'r');
+$str = fgets($stdin);
+
+
+if (strlen($str)>0) {
+  echo $str."\n";
+  folderize();
+}else{
+  echo "Nada, Bye.\n";
+  exit;
+}
+
+function folderize(){
+  $f = getcwd();
+  $files = opendir($f);
+  while (false !== ($entrada = readdir($files))) {
+      echo "$entrada\n";
+  }
+  echo $f."\n";
+}
+?>
