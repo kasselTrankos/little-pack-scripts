6ce64c1aeeb60f95751e092d6179e3e9f0f881e8
kasselTrankos <alvaro.touzon@gmail.com>
2016-03-31 20:14:07 +0200

    end with lines search

--- search.php
+++ search.php
@@ -5,19 +5,49 @@ $str = fgets($stdin);
 
 
 if (strlen($str)>0) {
-  echo $str."\n";
-  folderize();
+  folderize($str);
 }else{
   echo "Nada, Bye.\n";
   exit;
 }
 
-function folderize(){
+function folderize($str){
+  $str = str_replace(array("\n\r", "\n", "\r"), '', $str);
   $f = getcwd();
   $files = opendir($f);
-  while (false !== ($entrada = readdir($files))) {
-      echo "$entrada\n";
+  while (false !== ($file = readdir($files))) {
+      $contents = file_get_contents($file);
+      $pattern = preg_quote($str, '/');
+      $pattern = "/^.*$pattern.*\$/m";
+      if(preg_match_all($pattern, $contents, $matches)){
+        $founded[] = $file;
+      }
   }
-  echo $f."\n";
+  if(isset($founded)){
+    for($i=0; $i<count($founded); $i++){
+      $lines = getLineWithString($founded[$i], $str);
+      echo "encontrado \"$str\" en  \"$founded[$i]\n";
+      for($t=0; $t<count($lines); $t++){
+        echo "line:".$lines[$t][1]." --> ".$lines[$t][0]." <--- \n";
+      }
+
+    }
+
+  }else{
+    echo "$str nada no esta ;)\n";
+  }
+
+}
+function getLineWithString($fileName, $str) {
+    $lines = file($fileName);
+    $i = 1;
+    $founded=[];
+    foreach ($lines as $lineNumber => $line) {
+        if (strpos($line, $str) !== false) {
+            $founded[] = [str_replace(array("\n\r", "\n", "\r"), '', $line), $i];
+        }
+        $i++;
+    }
+    return $founded;
 }
 ?>
